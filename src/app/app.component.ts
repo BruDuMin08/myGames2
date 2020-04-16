012e57c3528876339803c9eebcb0d75304396b0c
BruDuMin08 <bsilva280217@gmail.com>
2020-04-16 15:12:42 -0300

    Listagem dos jogos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterModule, Router, NavigationEnd } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { filter, map } from 'rxjs/operators';
 import * as $ from 'jquery';
@@ -9,26 +9,27 @@ import * as $ from 'jquery';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
+  // Título da aplicação
   pageTitle = 'My Games';
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private titleService: Title
-  ) {}
+  ) { }
 
-  changeTitle(){
-
+  // Método que altera o título dinâmicamente conforme o valor de 'title' em 'app-routing.module.ts'.
+  changeTitle() {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(() => {
         let child = this.activatedRoute.firstChild;
-        while (child){
-          if (child.firstChild){
+        while (child) {
+          if (child.firstChild) {
             child = child.firstChild;
-          } else if (child.snapshot.data && child.snapshot.data.title){
+          } else if (child.snapshot.data && child.snapshot.data.title) {
             return `${child.snapshot.data.title} ~ ${this.pageTitle}`;
           } else {
             return this.pageTitle;
@@ -37,46 +38,60 @@ export class AppComponent implements OnInit{
         return this.pageTitle;
       })).subscribe((title: any) => {
         this.titleService.setTitle(title);
-        });
+      });
   }
+
+  // Controla a exibição do menu principal em resoluções mais baixas
   menuToggle() {
-    $(document).ready(() => {
+    $(document).ready(() => { // JQuery
 
-      if( $('#links').is(':visible') ) {
-        this.menuHide();
-      } else{
-        this.menuShow();
+      if ($('#links').is(':visible')) { // Se o menu está visível
+        this.menuHide(); // Chama o método que oculta o menu
+      } else { // Se o menu está oculto
+        this.menuShow(); // Chama o método que mostra o menu
       }
+
     });
-    return false;
+    return false; // Retorna sem fazer mais nada
   }
 
+  // Oculta o menu
   menuHide() {
     $('#links').slideUp('fast');
   }
 
-  menuShow(){
+  // Mostra o menu
+  menuShow() {
     $('#links').slideDown('fast');
   }
 
+  // Executa ao iniciar este componente
   ngOnInit(): void {
+
+    // Executa o método que altera o título
     this.changeTitle();
 
-    $(document).ready(() => {
+    //
+    $(document).ready(() => { // JQuery
+
+      // Quando a janela for redimensionada
       $(window).resize(() => {
-        if (window.innerWidth > 511){
-          $('#links').show(0);
-        } else {
-          $('#links').hide(0);
+        if (window.innerWidth > 511) { // Se a largura é maior que 511 px
+          $('#links').show(0); // Sempre mostra o menu
+        } else { // Se a largura é menor ou igual a 511 px
+          $('#links').hide(0); // Sempre oculta o menu
         }
       });
 
+      // Quando clicar em um item (rota) do menu
       $(document).on('click', '#links > a', () => {
-        $('#links').slideUp('fast');
+        if (window.innerWidth < 512) { // Se a largura é menor que 512 px
+          $('#links').slideUp('fast'); // Oculta o menu principal
+        }
       });
 
-
     });
-  }
-}
 
+  }
+
+}
